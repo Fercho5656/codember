9bb6d77a672a44b935762925c8a41354331660f1
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-11-15 16:03:25 -0600

    Add function to decode numbers to ASCII characters

---
+++ challenge_02/decode.js
@@ -0,0 +1,21 @@
+const decode = (message) => {
+  const words = message.split(' ')
+  const decoded = words.map(word => {
+    const chars = []
+    // const chars = word.match(/.{1,3}/g)
+    for (let i = 0; i < word.length; i += 0) {
+      if (word[i] === '1') {
+        chars.push(word[i] + word[i + 1] + word[i + 2])
+        i += 3
+      }
+      if (word[i] === '9') {
+        chars.push('0' + word[i] + word[i + 1])
+        i += 2
+      }
+    }
+    return chars.map(char => String.fromCharCode(parseInt(char))).reduce((a, b) => a + b)
+  })
+  return decoded.join(' ')
+}
+
+export default decode
